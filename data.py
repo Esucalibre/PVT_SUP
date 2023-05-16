62fb1eb23c6c3b65919699de018c6a47772c1df7
Esucalibre <josh.shah33d@gmail.com>
2023-05-16 14:18:54 -0400

    Shaheed's data struct

--- data.py
+++ data.py
@@ -1,5 +1,33 @@
-WEBSITE_LIST = {"https://www.youtube.com/":
+WEBSITE_LIST = {"https://www.lenovo.com":
                 {"specifics":"ablock=true;",
+                 "main_menu":'refresh_sens:id;guide-icon',
+                 "endpoints":
+                 { 
+                  "PC&Tablets":   'css selector;div.nav_item_title',
+                  "Servers":  'partial link text;SERVERS',
+                  "Devices": 'partial link text;SMART DEVICES'
+                  
+                 },
+                  "sub-endpoints":
+                  {
+                      "main_menu":{"trending":'relies_prev:partial link text;Trending',
+                                   "music":   'relies_prev:partial link text;Music',
+                                   "gaming":  'relies_prev:partial link text;Gaming',
+                                   "news":    'relies_prev:partial link text;News',
+                                   "sports":  'relies_prev:partial link text;Sports',
+                                   "learning":'relies_prev:partial link text;Learning'
+                                   },
+                      "trending":{"video":'rand_ind:css selector;ytd-video-renderer'
+                                  },
+                      "music":{"video":'rand_ind:css selector;ytd-video-renderer'
+                               },
+                      "gaming":{"video":"rand_ind:css selector;ytd-video-renderer"
+                          
+                      },
+                  }
+                 },
+                 "next_website":
+                 {"specifics":"ablock=true;",
                  "main_menu":'refresh_sens:id;guide-icon',
                  "endpoints":
                  { 
