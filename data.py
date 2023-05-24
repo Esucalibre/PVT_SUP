8174ff4e66703bf07b8ac267d7a7bf1f0f7cd25f
Esucalibre <josh.shah33d@gmail.com>
2023-05-24 09:50:12 -0400

    updated

--- data.py
+++ data.py
@@ -55,15 +55,18 @@ WEBSITE_LIST = {"https://www.lenovo.com":
                         },
                       },
                   }
-                 },
-                
-                {"www.intel.com":
+                 }
+
+WEBSITE_LIST = {"https://www.intel.com":
                  {"specifics":"ablock=true;",
                  "main_menu":'refresh_sens:id;guide-icon',
                  "endpoints":
                  { 
-                  "login-page":   'direct-link;signin',
-                  "video":  'css selector;ytd-rich-item-renderer'
+                  'Products'
+                  'Support'
+                  'Solutions'
+                  'Developers'
+                  'Partners'
                  },
                   "sub-endpoints":
                   {
@@ -83,37 +86,4 @@ WEBSITE_LIST = {"https://www.lenovo.com":
                       },
                   }
                  }
-                },
-
-
-
-{"next site":
-                 {"specifics":"ablock=true;",
-                 "main_menu":'refresh_sens:id;guide-icon',
-                 "endpoints":
-                 { 
-                  "login-page":   'direct-link;signin',
-                  "video":  'css selector;ytd-rich-item-renderer'
-                 },
-                  "sub-endpoints":
-                  {
-                      "main_menu":{"trending":'relies_prev:partial link text;Trending',
-                                   "music":   'relies_prev:partial link text;Music',
-                                   "gaming":  'relies_prev:partial link text;Gaming',
-                                   "news":    'relies_prev:partial link text;News',
-                                   "sports":  'relies_prev:partial link text;Sports',
-                                   "learning":'relies_prev:partial link text;Learning'
-                                   },
-                      "trending":{"video":'rand_ind:css selector;ytd-video-renderer'
-                                  },
-                      "music":{"video":'rand_ind:css selector;ytd-video-renderer'
-                               },
-                      "gaming":{"video":"rand_ind:css selector;ytd-video-renderer"
-                          
-                      },
-                  }
-                 }
-                },
-
-
-
+                }
